cdba820a88ba460e8bd8823cedf1737d163e4178
DeanFaizal <dean.faizal@microsoft.com>
2019-07-30 16:29:59 -0400

    Initial app & wireup of MobCAT MVVM

---
+++ XamTwitch/XamTwitch.UITests/Pages/NewItemPage.cs
@@ -0,0 +1,55 @@
+﻿using Xamarin.UITest;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace XamTwitch.UITests
+{
+    public class NewItemPage : BasePage
+    {
+        readonly Query itemNameEntry, itemDescriptionEditor, saveToolbarItem;
+
+        public NewItemPage(IApp app, Platform platform) : base(app, platform, "New Item")
+        {
+            if (OniOS)
+            {
+                itemNameEntry = x => x.Class("UITextField").Index(0);
+                itemDescriptionEditor = x => x.Class("UITextView").Index(0);
+            }
+            else
+            {
+                itemNameEntry = x => x.Class("FormsEditText").Index(0);
+                itemDescriptionEditor = x => x.Class("FormsEditText").Index(1);
+            }
+
+            saveToolbarItem = x => x.Marked("Save");
+        }
+
+        public void EnterItemName(string text)
+        {
+            EnterText(itemNameEntry, text);
+
+            app.Screenshot("Entered Item Name");
+        }
+
+        public void EnterItemDescription(string text)
+        {
+            EnterText(itemDescriptionEditor, text);
+
+            app.Screenshot("Entered Item Description");
+        }
+
+        public void TapSaveToolbarButton()
+        {
+            app.Tap(saveToolbarItem);
+
+            app.Screenshot("Save Toolbar Item Tapped");
+        }
+
+        void EnterText(Query textBoxQuery, string text)
+        {
+            app.ClearText(textBoxQuery);
+            app.EnterText(textBoxQuery, text);
+            app.DismissKeyboard();
+        }
+    }
+}
---
+++ XamTwitch/XamTwitch/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XamTwitch
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
