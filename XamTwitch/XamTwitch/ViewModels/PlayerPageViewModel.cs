4a18ed81143343f79d6e5a8757d312ee2f21d0ad
Alexey Strakh <alexey.strakh@microsoft.com>
2019-09-10 14:45:59 -0700

    proper threading logic for data access

--- XamTwitch/XamTwitch/ViewModels/PlayerPageViewModel.cs
+++ XamTwitch/XamTwitch/ViewModels/PlayerPageViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.MobCAT.MVVM;
 using Newtonsoft.Json;
+using Xamarin.Essentials;
 using XamTwitch.Models;
 
 namespace XamTwitch.ViewModels
@@ -18,10 +20,7 @@ namespace XamTwitch.ViewModels
             {
                 if(RaiseAndUpdate(ref _stream, value))
                 {
-                    FetchTwitchStreamAsync().ContinueWith(t =>
-                    {
-                        Raise(nameof(StreamSource));
-                    });
+                    FetchTwitchStreamAsync();
                 }
             }
         }
@@ -44,11 +43,12 @@ namespace XamTwitch.ViewModels
                 if (Stream == null)
                     return;
 
+                System.Diagnostics.Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, IsMain: {MainThread.IsMainThread}");
                 var channelName = Stream.UserName.ToLower();
                 var httpClient = new HttpClient();
                 httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Client-ID", Constants.TwitchApiKey);
                 var tokenUrl = $"https://api.twitch.tv/api/channels/{channelName}/access_token";
-                var tokenResponse = await httpClient.GetStringAsync(tokenUrl);
+                var tokenResponse = await httpClient.GetStringAsync(tokenUrl).ConfigureAwait(false);
                 var token = JsonConvert.DeserializeObject<TwitchToken>(tokenResponse);
 
                 if (string.IsNullOrWhiteSpace(token.Sig) || string.IsNullOrWhiteSpace(token.Token))
@@ -60,14 +60,20 @@ namespace XamTwitch.ViewModels
                 var playlistUrl = $"https://usher.ttvnw.net/api/channel/hls/{channelName}.m3u8?sig={token.Sig}&token={tokenUpdated}";
                 System.Diagnostics.Debug.WriteLine(playlistUrl);
 
-                var playlistReponse = await httpClient.GetStringAsync(playlistUrl);
+                var playlistReponse = await httpClient.GetStringAsync(playlistUrl).ConfigureAwait(false);
 
                 var match = Regex.Match(playlistReponse, @"(?<stream>https://(.+).m3u8)");
                 if(!match.Success)
                     throw new InvalidOperationException();
 
                 var streamUrl = match.Groups["stream"].Value;
-                StreamSource = streamUrl;
+
+                System.Diagnostics.Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId}, IsMain: {MainThread.IsMainThread}");
+                // need UI
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    StreamSource = streamUrl;
+                });
             }
             catch (Exception ex)
             {
