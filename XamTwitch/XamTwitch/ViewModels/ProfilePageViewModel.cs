41ed05b409b7622daa20ce1d75fffc1d73ac3768
Sweekriti Satpathy <swsat@microsoft.com>
2019-10-23 13:00:22 -0700

    ProfilePage added

---
+++ XamTwitch/XamTwitch/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.MobCAT.MVVM;
+
+namespace XamTwitch.ViewModels
+{
+    public class ProfilePageViewModel : BaseNavigationViewModel
+    {
+        public ProfilePageViewModel()
+        {
+        }
+    }
+}
