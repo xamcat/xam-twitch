7d1d526abfeffbb7f96c31cfa373e0bf596646f3
Sweekriti Satpathy <swsat@microsoft.com>
2019-11-25 19:40:03 -0800

    adds interface for auth service

---
+++ XamTwitch/XamTwitch/Services/ITwitchAuthHttpService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XamTwitch.Models;
+
+namespace XamTwitch.Services
+{
+    public interface ITwitchAuthHttpService
+    {
+        Task<TwitchUser> GetUserAsync();
+    }
+}
